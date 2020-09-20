836589f8fbd000627a35cc5b1d434be9bbc2b46f
eduardo <edu_felip@hotmail.com>
2020-09-19 21:19:12 -0300

    new job, exclusion account email

---
+++ src/lib/jobs/ExclusionEmail.ts
@@ -0,0 +1,12 @@
+import { MailtrapMailProvider } from '../../providers/implementations/MailtrapMailProvider'
+import { IMessage } from '../../providers/IMailProvider'
+
+const mailtrapMailProvider = new MailtrapMailProvider()
+
+export default {
+  key: 'ExclusionMail',
+  options: { priority: 1 },
+  async handle(message: IMessage): Promise<void> {
+    await mailtrapMailProvider.sendMail(message)
+  }
+}
