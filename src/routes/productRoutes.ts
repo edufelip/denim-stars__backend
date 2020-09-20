d3ac682a906cadd229c07d45317028e215c71a9e
eduardo <edu_felip@hotmail.com>
2020-09-19 21:18:28 -0300

    routes properly set

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,9 +1,36 @@
+import { createProductController } from '@controllers/createProduct'
+import { deleteUserController } from '@controllers/deleteUser'
+import { updateUserController } from '@controllers/updateUser'
 import { Router } from 'express'
 
 const router = Router()
 
 router.get('/', (req, res) => {
-  return res.json('foi')
+  return res.json('list all products')
+})
+
+router.get('/new', (req, res) => {
+  return res.json('new product form')
+})
+
+router.post('/', (req, res) => {
+  return createProductController.handle(req, res)
+})
+
+router.get('/:id', (req, res) => {
+  return res.json('show certain product')
+})
+
+router.get('/:id/edit', (req, res) => {
+  return res.json('edit form to single product')
+})
+
+router.put('/:id', (req, res) => {
+  return updateUserController.handle(req, res)
+})
+
+router.delete('/:id', (req, res) => {
+  return deleteUserController.handle(req, res)
 })
 
 export { router }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
 import { createUserController } from '@controllers/createUser'
+import { updateUserController } from '@controllers/updateUser'
+import { deleteUserController } from '@controllers/deleteUser'
+import User from '@schemas/User'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  return res.json('list all users')
+router.get('/', async (req, res) => {
+  const users = await User.find()
+  return res.json(users)
 })
 
 router.get('/new', (req, res) => {
@@ -15,20 +19,22 @@ router.post('/', (req, res) => {
   return createUserController.handle(req, res)
 })
 
-// router.get('/:id', (req, res) => {
-//   return res.json('show certain user')
-// })
+router.get('/:id', async (req, res) => {
+  const id = req.params.id
+  const user = await User.findById(id)
+  return res.json(user)
+})
 
-// router.get('/:id/edit', (req, res) => {
-//   return res.json('edit form to single user')
-// })
+router.get('/:id/edit', (req, res) => {
+  return res.json('edit form to single user')
+})
 
-// router.put('/:id', (req, res) => {
-//   return res.json('update certain user')
-// })
+router.put('/:id', (req, res) => {
+  return updateUserController.handle(req, res)
+})
 
-// router.delete('/:id', (req, res) => {
-//   return res.json('delete certain user')
-// })
+router.delete('/:id', (req, res) => {
+  return deleteUserController.handle(req, res)
+})
 
 export { router }
